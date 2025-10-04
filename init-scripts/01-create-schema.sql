8ae0179c9a80361bb114fbe5e461fe069cb02683
Abdullah Habeeb <ahabeeb2081@gmail.com>
2025-10-03 23:43:37 -0400

    building dim structure

--- init-scripts/01-create-schema.sql
+++ init-scripts/01-create-schema.sql
@@ -36,3 +36,75 @@ CREATE TABLE staging.taxi_zone_lookup (
     zone TEXT,
     service_zone TEXT
 );
+
+CREATE TABLE warehouse.dim_taxi_zone (
+    zone_key SERIAL PRIMARY KEY,
+    location_id INTEGER,
+    borough TEXT,
+    zone TEXT,
+    service_zone TEXT,
+    valid_from TIMESTAMP,
+    valid_to TIMESTAMP,
+    is_current BOOLEAN,
+    created_at TIMESTAMP DEFAULT NOW()
+)
+
+CREATE TABLE warehouse.dim_vendor (
+    vendor_key SERIAL PRIMARY KEY,
+    vendor_lk_id INTEGER,
+    vendor_name TEXT,
+    valid_from TIMESTAMP,
+    valid_to TIMESTAMP,
+    is_current BOOLEAN,
+    created_at TIMESTAMP DEFAULT NOW()
+)
+
+CREATE TABLE warehouse.dim_payment_type (
+    payment_type_key SERIAL PRIMARY KEY,
+    payment_type_lk_id INTEGER,
+    payment_type_name TEXT,
+    valid_from TIMESTAMP,
+    valid_to TIMESTAMP,
+    is_current BOOLEAN,
+    created_at TIMESTAMP DEFAULT NOW()
+)
+
+CREATE TABLE warehouse.dim_ratecode (
+    ratecode_key SERIAL PRIMARY KEY,
+    ratecode_lk_id INTEGER,
+    ratecode_name TEXT,
+    valid_from TIMESTAMP,
+    valid_to TIMESTAMP,
+    is_current BOOLEAN,
+    created_at TIMESTAMP DEFAULT NOW()
+)
+
+
+CREATE TABLE warehouse.yellow_tripdata_2023 (
+    trip_key SERIAL PRIMARY KEY,
+    
+    -- Dimension foreign keys
+    vendor_key INTEGER REFERENCES warehouse.dim_vendor(vendor_key),
+    pu_zone_key INTEGER REFERENCES warehouse.dim_taxi_zone(zone_key),
+    do_zone_key INTEGER REFERENCES warehouse.dim_taxi_zone(zone_key),
+    ratecode_key INTEGER REFERENCES warehouse.dim_ratecode(ratecode_key),
+    payment_type_key INTEGER REFERENCES warehouse.dim_payment_type(payment_type_key),
+    -- Temporal columns (for point-in-time joins)
+    tpep_pickup_datetime TIMESTAMP NOT NULL,
+    tpep_dropoff_datetime TIMESTAMP NOT NULL,
+    -- Facts/Measures
+    passenger_count INTEGER,
+    trip_distance DECIMAL(8,2),
+    fare_amount DECIMAL(10,2),
+    extra DECIMAL(10,2),
+    mta_tax DECIMAL(10,2),
+    tip_amount DECIMAL(10,2),
+    tolls_amount DECIMAL(10,2),
+    improvement_surcharge DECIMAL(10,2),
+    total_amount DECIMAL(10,2),
+    congestion_surcharge DECIMAL(10,2),
+    airport_fee DECIMAL(10,2),
+    -- Audit columns
+    loaded_at TIMESTAMP DEFAULT NOW(),
+    source_file TEXT
+);
